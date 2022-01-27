2801d078d4fc0aff584d7a062c5133f5f9c7a898
rppicomidi <rppicomidi@gmail.com>
2022-01-27 15:44:01 -0800

    Remove dependency on hardware_i2c from encoder-pio library

--- ext_lib/encoder-pio/encoder-pio.cmake
+++ ext_lib/encoder-pio/encoder-pio.cmake
@@ -9,4 +9,4 @@ pico_generate_pio_header(encoder-pio ${CMAKE_CURRENT_LIST_DIR}/encoder.pio)
 target_include_directories(encoder-pio INTERFACE ${CMAKE_CURRENT_LIST_DIR})
 
 # Pull in pico libraries that we need
-target_link_libraries(encoder-pio INTERFACE pico_stdlib hardware_i2c)
+target_link_libraries(encoder-pio INTERFACE pico_stdlib)
